29b91cf0def1a967280cd03c4724b53cc212a268
Ega Arya <egaarya99@mail.ugm.ac.id>
2018-10-31 10:12:27 +0700

    second commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -84,7 +84,7 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-6">
-            <h2>About The Event</h2>
+            <h2>About Sementara</h2>
             <p>Sed nam ut dolor qui repellendus iusto odit. Possimus inventore eveniet accusamus error amet eius aut
               accusantium et. Non odit consequatur repudiandae sequi ea odio molestiae. Enim possimus sunt inventore in
               est ut optio sequi unde.</p>
@@ -102,23 +102,44 @@
     </section>
 
     <!--==========================
-      Speakers Section
+      Consultation Section
     ============================-->
-    
+    <section id="consultation">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h2>Colsultation Sementara</h2>
+            <p>Sed nam ut dolor qui repellendus iusto odit. Possimus inventore eveniet accusamus error amet eius aut
+              accusantium et. Non odit consequatur repudiandae sequi ea odio molestiae. Enim possimus sunt inventore in
+              est ut optio sequi unde.</p>
+          </div>
+        </div>
+      </div>
+    </section>
 
     <!--==========================
-      Schedule Section
+      The Motivate Section
     ============================-->
-    
-          <!-- End Schdule Day 1 -->
-
-          <!-- Schdule Day 2 -->
-          
-          <!-- End Schdule Day 2 -->
-
-          <!-- Schdule Day 3 -->
-          
-          <!-- End Schdule Day 2 -->
+    <section id="motive">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3">
+            <h3>Where</h3>
+            <p>Downtown Conference Center, New York</p>
+          </div>
+          <div class="col-lg-3">
+            <h3>When</h3>
+            <p>Monday to Wednesday<br>10-12 December</p>
+          </div>
+          <div class="col-lg-6">
+            <h2>The Motivate Sementara</h2>
+            <p>Sed nam ut dolor qui repellendus iusto odit. Possimus inventore eveniet accusamus error amet eius aut
+              accusantium et. Non odit consequatur repudiandae sequi ea odio molestiae. Enim possimus sunt inventore in
+              est ut optio sequi unde.</p>
+          </div>
+        </div>
+      </div>
+    </section>
 
       
     <!--==========================
