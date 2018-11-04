98a1eaf0eeb0da297bba329050aa05c139b52bd4
unknown <jaffar.jatmiko.jati@mail.ugm.ac.id>
2018-11-04 16:35:30 +0700

    login register

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,9 @@ Route::get('motive', function () {
 Route::get('shareW', function () {
     return view('shareW');
 });
+Route::get('login', function () {
+    return view('login');
+});
+Route::get('register', function () {
+    return view('register');
+});
